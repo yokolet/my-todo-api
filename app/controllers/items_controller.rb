cc1a548020bd8009f72f657dae3d997be279c171
Yoko Harada <yokolet@gmail.com>
2017-09-09 20:29:20 +0900

    adds show method and its tests

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,11 @@ class ItemsController < ApplicationController
     @items = @todo.items if @todo
     render json: @items, status: :ok
   end
+
+  def show
+    @todo = Todo.find(params[:todo_id])
+    @item = @todo.items.find_by!(id: params[:id]) if @todo
+    render json: @item, status: :ok
+  end
   
 end
--- test/integration/item_flows_test.rb
+++ test/integration/item_flows_test.rb
@@ -9,7 +9,7 @@ class ItemFlowsTest < ActionDispatch::IntegrationTest
   end
 
   # GET /todos/:todo_id/items
-  test "get /todos/todo_id/items" do
+  test "get /todos/:todo_id/items" do
     get "/todos/#{@todo_id}/items"
     assert_response :success
 
@@ -17,11 +17,28 @@ class ItemFlowsTest < ActionDispatch::IntegrationTest
     assert_equal(20, data.size)
   end
 
-  # GET todos/:id/items with wrong id
+  # GET todos/:todo_id/items with wrong todo_id
   test "get /todos/100/items" do
     wrong_id = 100
     get "/todos/#{wrong_id}/items"
-    assert_response 404
+    assert_response :not_found
     assert_match(/Couldn't find Todo/, @response.body)
+  end
+
+  # GET /todos/:todo_id/items/:id
+  test "get /todos/:todo_id/items/:id" do
+    get "/todos/#{@todo_id}/items/#{@item_id}"
+    assert_response :success
+
+    data = JSON.parse(@response.body)
+    assert_equal(@item_id, data["id"])
+  end
+
+  # GET /todos/:todo_id/items/:id with wrong id
+  test "get /todos/:todo_id/items/1000" do
+    wrong_id = 1000
+    get "/todos/#{@todo_id}/items/#{wrong_id}"
+    assert_response :not_found
+    assert_match(/Couldn't find Item/, @response.body)
   end
 end
